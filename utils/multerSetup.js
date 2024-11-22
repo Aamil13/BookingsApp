2c5c221b431573d033ee5afaf3dcd1fd4603a56b
Aamil13 <aamil.shafi13@gmail.com>
2024-11-22 15:54:54 +0530

    added multer temp storage wrt os

--- utils/multerSetup.js
+++ utils/multerSetup.js
@@ -1,15 +1,30 @@
 import multer from "multer";
-
+import os from 'os';
 //Multer Configuration
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// export {upload}
+
+// for vercel /tmp works
+const tempDir = os.tmpdir()
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, tempDir); // Use system's temp directory
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname); // Keep the original filename
   },
 });
 
 const upload = multer({ storage: storage });
 
-export {upload}
+export { upload };
