21b020aa2a68becd3949fbe8c5c794cc471c6d30
Aamil13 <aamil.shafi13@gmail.com>
2024-11-22 16:26:13 +0530

    test cloudinary api

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -9,4 +9,20 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+
+console.log('Cloudinary Config:', {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Test the configuration
+cloudinary.v2.api.resources((error, result) => {
+  if (error) {
+    console.error('Cloudinary Error:', error.message);
+  } else {
+    console.log('Cloudinary Resources:', result.resources);
+  }
+});
+
 export default cloudinary;
